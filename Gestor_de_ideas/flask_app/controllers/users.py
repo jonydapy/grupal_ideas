ecd5f7b1a273a0616b2408ab215ac661c1e208f3
Jonattan Da Silva <jonydasilvapy@gmail.com>
2023-03-14 20:00:18 -0300

    update users

--- Gestor_de_ideas/flask_app/controllers/users.py
+++ Gestor_de_ideas/flask_app/controllers/users.py
@@ -27,18 +27,6 @@ def register():
             flash('Passwords must be the same!')
     return render_template('register.html')
 
-    """ if not User.validate_register(request.form):
-        return redirect('/')
-    data ={ 
-        "first_name": request.form['first_name'],
-        "last_name": request.form['last_name'],
-        "email": request.form['email'],
-        "password": bcrypt.generate_password_hash(request.form['password'])
-    }
-    id = User.save(data)
-    session['user_id'] = id
-    return redirect('/dashboard') """
-
 @app.route('/log-user', methods = ['GET','POST'])
 def log_user():
     if request.method == 'POST':
@@ -53,16 +41,6 @@ def log_user():
         return redirect('/dashboard')
     else:
         return redirect('/')
-    
-    """ user = User.get_by_email(request.form)
-    if not user:
-        flash("Invalid Email","login")
-        return redirect('/')
-    if not bcrypt.check_password_hash(user.password, request.form['password']):
-        flash("Invalid Password","login")
-        return redirect('/')
-    session['user_id'] = user.id
-    return redirect('/dashboard') """
 
 @app.route('/dashboard') # Jony
 def dashboard():
