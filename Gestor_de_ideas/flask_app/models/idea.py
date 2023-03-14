5a762710447c82777cb1268a000bf24010cf6a6b
joelmm11 <joel.mm@hotmail.com>
2023-03-13 21:37:54 -0300

    actualizacion modelo idea

--- Gestor_de_ideas/flask_app/models/idea.py
+++ Gestor_de_ideas/flask_app/models/idea.py
@@ -1,23 +1,22 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 
-class Initiative:
+class Idea:
     db_name = "ideas"
     def __init__(self,data):
-        self.idinitiative = data['idinitiative']
-        self.name = data['name']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+        self.ididea = data['ididea']
+        self.content = data['content']
+        self.user_id = data['user_id']
         self.id_cluster = data['id_cluster']
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO initiative (name, created_at, updated_at, id_cluster) VALUES (%(name)s, NOW(), NOW(), %(id_cluster)s);"
+        query = "INSERT INTO ideas (content, user_id, id_cluster) VALUES (%(content)s,%(user_id)s,%(id_cluster)s);"
         return connectToMySQL(cls.db_name).query_db(query,data)
 
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM initiative;"
+        query = "SELECT * FROM ideas;"
         results = connectToMySQL(cls.db_name).query_db(query)
         initiatives = []
         for row in results:
@@ -26,14 +25,14 @@ class Initiative:
 
     @classmethod
     def get_by_id(cls,data):
-        query = "SELECT * FROM initiative WHERE idinitiative = %(idinitiative)s;"
+        query = "SELECT * FROM ideas WHERE ididea = %(ididea)s;"
         results = connectToMySQL(cls.db_name).query_db(query,data)
         return cls(results[0])
 
-    @staticmethod
-    def validate_initiative(initiative):
-        is_valid = True
-        if len(initiative['name']) < 5:
-            flash("Initiative name must be at least 5 character")
-            is_valid= False
-        return is_valid
+#    @staticmethod
+#    def validate_initiative(initiative):
+#        is_valid = True
+#        if len(initiative['name']) < 5:
+#            flash("Initiative name must be at least 5 character")
+#            is_valid= False
+#        return is_valid
