65256e76977a042764f50fd81c9bf0626d26f3e2
mathvillalba <118029867+mathvillalba@users.noreply.github.com>
2023-03-14 23:12:10 -0300

    cambios varios controlador/modelo/ html
    Cambios varios en horario de ayudantia

--- Gestor_de_ideas/flask_app/models/idea.py
+++ Gestor_de_ideas/flask_app/models/idea.py
@@ -7,11 +7,13 @@ class Idea:
         self.ididea = data['ididea']
         self.content = data['content']
         self.user_id = data['user_id']
+        self.id_campaing = data['id_campaing']
         self.id_cluster = data['id_cluster']
+        self.comment = data['comment']
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO ideas (content, user_id, id_cluster) VALUES (%(content)s,%(user_id)s,%(id_cluster)s);"
+        query = "INSERT INTO ideas (content, user_id, id_campaing, id_cluster) VALUES (%(content)s,%(user_id)s,%(id_campaing)s,%(id_cluster)s);"
         return connectToMySQL(cls.db_name).query_db(query,data)
 
     @classmethod
@@ -24,10 +26,16 @@ class Idea:
         return initiatives
 
     @classmethod
-    def get_by_id(cls,data):
-        query = "SELECT * FROM ideas WHERE ididea = %(ididea)s;"
+    def get_by_ididea(cls,data):
+        query = "SELECT * FROM ideas WHERE ididea = %(ididea)s;" 
+        #hacer join para traer nombre del usuario ej: SELECT distinct(si se repiten nombres) m.grupo_id, u.first_name, u.last_name FROM miembros m JOIN users u ON m.user_id=u.id order by m.grupo_id;"
         results = connectToMySQL(cls.db_name).query_db(query,data)
-        return cls(results[0])
+        return (results[0])
+    @classmethod
+    def save_comment(cls,data):
+        query = "insert into comments (comment, user_id, ididea ) values (%(comment)s,%(user_id)s,%(ididea)s)"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+        
 
 #    @staticmethod
 #    def validate_initiative(initiative):
