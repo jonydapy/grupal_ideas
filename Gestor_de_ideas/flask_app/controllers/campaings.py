4a781672314f5c22e2789efaf79362bb21174ce8
mathvillalba <118029867+mathvillalba@users.noreply.github.com>
2023-03-15 21:12:36 -0300

    serverpy, y controlador campaing

--- Gestor_de_ideas/flask_app/controllers/campaings.py
+++ Gestor_de_ideas/flask_app/controllers/campaings.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, session, request, flash
 from flask_app import app
 from flask_app.models.user import User
 from flask_app.models.idea import Idea
+from flask_app.models.campaing import Campaign
 from flask_app.models.initiative import Initiative
 from flask_app.models.type_users import Type_User
 #from flask_app.models.agendamiento import Agendamiento
@@ -10,4 +11,4 @@ bcrypt = Bcrypt(app)
 
 @app.route('/vercampaigns')
 def vercampaigns():
-    return render_template('campaings.html', campaings= Idea.get_allcampaing())
+    return render_template('campaings.html', campaings= Campaign.get_allcampaing())
--- Gestor_de_ideas/server.py
+++ Gestor_de_ideas/server.py
@@ -1,4 +1,4 @@
 from flask_app import app
-from flask_app.controllers import users,ideas
+from flask_app.controllers import users,ideas, campaings
 if __name__=="__main__":
     app.run(debug=True)
