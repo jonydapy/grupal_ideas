fb3e756dd945dd03558473d01322a2c1620b707f
Jonattan Da Silva <jonydasilvapy@gmail.com>
2023-03-15 22:26:00 -0300

    modificacion de server y initiatives

---
+++ Gestor_de_ideas/flask_app/controllers/initiatives.py
@@ -0,0 +1,29 @@
+from flask import render_template, redirect, session, request, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.initiative import Initiative
+from flask_app.models.type_users import Type_User
+from flask_app.models.cluster import Cluster
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/initiative/create') # Jony
+def initiative():
+    if session.get('id') == None:
+        return redirect('/')
+    if session.get('type_user') == 1 or session.get('type_user') == 4:
+        user_data = User.get_by_id(session)
+        print(Cluster.get_all())
+        return  render_template('create_initiative.html', all_clusters = Cluster.get_all(), user_data = user_data)
+    else:
+        return redirect('/')
+    
+@app.route('/initiative/create-now', methods = ['GET','POST']) # Jony
+def create_initiative():
+    if request.method == 'POST':
+        data = dict(request.form)
+        Initiative.save(data)
+        return redirect('/initiative/create')
+    return redirect('/')
+
+
--- Gestor_de_ideas/server.py
+++ Gestor_de_ideas/server.py
@@ -1,4 +1,4 @@
 from flask_app import app
-from flask_app.controllers import users,ideas, campaings
+from flask_app.controllers import users,ideas, campaings, initiatives
 if __name__=="__main__":
     app.run(debug=True)
